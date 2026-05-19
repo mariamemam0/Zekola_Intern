4ed250be6904d7ebbcd7edc101e0472c86a56e58
marim <mariamemampp00@gmail.com>
2026-05-19 16:25:33 +0300

    make collections for product and order

---
+++ app/Http/Resources/OrderCollection.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\ResourceCollection;
+
+class OrderCollection extends ResourceCollection
+{
+    /**
+     * Transform the resource collection into an array.
+     *
+     * @return array<int|string, mixed>
+     */
+
+    public $collects = OrderResource::class;
+
+    public function toArray(Request $request): array
+    {
+        return[
+            'products' => $this->collection,
+            'count'    => $this->count(),
+        ];
+    }
+}
