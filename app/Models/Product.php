96af0393b3f300192275db22f6887ac7aaaf33fb
marim <mariamemampp00@gmail.com>
2026-05-20 17:52:37 +0300

    add accessor method to add $ to the price

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
@@ -15,5 +16,12 @@ class Product extends Model
     public function orders()
     {
         return $this->hasMany(Order::class);
+    }
+
+    protected function price(): Attribute
+    {
+        return Attribute::make(
+            get: fn (float $value) => '$' . number_format($value, 2), // 999.9 → "$999.90"
+        );
     }
 }
